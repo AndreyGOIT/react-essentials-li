de80249f5abcc894b544d76fee49801622ab0298
AndreyGOIT <ae.borga@gmail.com>
2024-11-15 16:02:32 +0200

    destructuring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
-function App() {
+const [firstCity, rest] = [
+  "New York",
+  "Los Angeles",
+  "Chicago",
+  "Houston",
+  "Phoenix",
+];
+console.log(firstCity);
+console.log(rest);
+function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
