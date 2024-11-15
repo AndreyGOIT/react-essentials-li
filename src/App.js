4c94be0119387181ecf84544ab99229ef28bcb4a
AndreyGOIT <ae.borga@gmail.com>
2024-11-15 16:14:00 +0200

    using useState hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { useState } from 'react';
 
-function App({ library }) {
-  const what = useState();
-  console.log(what);
+function App() {
+  const [emotion, setEmotion] = useState("happy");
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
