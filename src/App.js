e16cd9fada84f86d3a1abf7c23ad6dbb2f80dc61
AndreyGOIT <ae.borga@gmail.com>
2024-11-16 11:33:39 +0200

    displaying data from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+function GitHubUser({name, location, avatar}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>Location: {location}</p>
+      <img src={avatar} height={150} alt={name}/>
+    </div>
+  );
+}
+
 function App() {
   const [data, setData] = useState(null);
   console.log(data);
@@ -11,9 +21,10 @@ function App() {
   }, []);
   if (data) {
     return (
-      <pre>
-        {JSON.stringify(data, null, 2)}
-      </pre>
+      <GitHubUser 
+      name={data.name}
+      location={data.location}
+      avatar={data.avatar_url} />
     );
   }
   return (
